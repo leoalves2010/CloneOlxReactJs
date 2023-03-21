f85a4393f9fee779936402a890bbe929f693c6f3
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-03-20 21:18:23 -0300

    Área de SearchBox da página inicial

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { PageArea, SearchArea } from "./styled";
+import { PageContainer } from "../../components/MainComponents";
+import { Api } from "../../components/helpers/Api";
 
 const Home = () => {
-    return <div>Página Inicial</div>;
+    return (
+        <>
+            <SearchArea>
+                <PageContainer>
+                    <div className="searchBox">
+                        <form method="get" action="/ads">
+                            <input
+                                type="text"
+                                name="q"
+                                placeholder="O que você procura?"
+                            />
+                            <select name="state"></select>
+                            <button>Pesquisar</button>
+                        </form>
+                    </div>
+                    <div className="categoryList"></div>
+                </PageContainer>
+            </SearchArea>
+            <PageContainer>
+                <PageArea>container</PageArea>
+            </PageContainer>
+        </>
+    );
 };
 
 export default Home;
---
+++ app/src/pages/Home/styled.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const SearchArea = styled.div`
+    background-color: #ddd;
+    border-bottom: #ccc;
+    padding: 20px 0;
+
+    .searchBox {
+        background-color: #9bb83c;
+        padding: 20px 15px;
+        border-radius: 5px;
+        box-shadow: 1px 1px 1px 0.3px rgba(0, 0, 0, 0.2);
+        display: flex;
+
+        form {
+            display: flex;
+            flex: 1;
+
+            input,
+            select {
+                height: 40px;
+                border: 0;
+                border-radius: 5px;
+                outline: 0;
+                font-size: 15px;
+                color: #000;
+                margin-right: 20px;
+            }
+
+            input {
+                flex: 1;
+                padding: 0 10px;
+            }
+
+            select {
+                width: 100px;
+            }
+
+            button {
+                background-color: #49aeef;
+                font-size: 15px;
+                border: 0;
+                border-radius: 5px;
+                color: #fff;
+                height: 40px;
+                padding: 0 20px;
+                cursor: pointer;
+
+                &:hover {
+                    background-color: #4098d0;
+                }
+            }
+        }
+    }
+`;
+export const PageArea = styled.div``;
